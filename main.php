f7c50c3312f3e5fa230597527f0b0fd87e6cea97
Simon DELAGE <sdelage@gmail.com>
2017-03-05 17:10:50 +0100

    Blockquote & WPSnippet plugins improvements
    Fixed interaction with ToC and added consistency between the two.

--- main.php
+++ main.php
@@ -23,7 +23,7 @@ $showSidebar = page_findnearest($conf['sidebar']);
     <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
     <?php tpl_includeFile('meta.html') ?>
   </head>
-  <body id="dokuwiki__top" class="<?php echo tpl_classes(); ?>">
+  <body id="dokuwiki__top" class="<?php echo tpl_classes();?><?php echo ($showSidebar) ? ' hasSidebar' : ''; ?>">
     <?php tpl_includeFile('header.html') ?>
     <!-- ********** HEADER ********** -->
     <header id="dokuwiki__header" role="banner" class="pam">
