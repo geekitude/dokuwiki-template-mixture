307e53a7432b6a3092aa4bad3965d4df244109dc
Simon DELAGE <sdelage@gmail.com>
2017-03-18 00:56:51 +0100

    Collapsing/expanding ToC and Sidebar done
    Mostly copied from Dokuwiki template (just a little more more
    complexified for extracted asides)

--- script.js
+++ script.js
@@ -1,28 +1,104 @@
 /**
  *  We handle several device classes based on browser width.
  *
- *  - desktop:   > __tablet_width__ (as set in style.ini)
+ *  - desktop:   > 1201px
  *  - mobile:
- *    - tablet   <= __tablet_width__
- *    - phone    <= __phone_width__
+ *    - tablet   >= 544px
+ *    - phone    <= 543px
+ *  And a special state when ToC and/or Sidebar are "extracted"
  */
+var device_class = ''; // not yet known
+var device_classes = 'extractedtoc extractedsb desktop mobile tablet phone';
+
+
+function mixture_mobile(){
+
+    // the z-index in mobile.css is (mis-)used purely for detecting the screen mode here
+    var screen_mode = jQuery('#screen__mode').css('z-index') + '';
+
+    // determine our device pattern
+    // TODO: consider moving into dokuwiki core
+    switch (screen_mode) {
+        case '1000':
+            if (device_class.match(/phone/)) return;
+            device_class = 'mobile phone';
+            break;
+        case '1001':
+            if (device_class.match(/tablet/)) return;
+            device_class = 'mobile tablet';
+            break;
+        case '2001':
+        case '2002':
+            device_class = 'extracted desktop';
+            break;
+        default:
+            if (device_class == 'desktop') return;
+            device_class = 'desktop';
+    }
+
+    jQuery('html').removeClass(device_classes).addClass(device_class);
+
+    // handle some layout changes based on change in device
+    var $aside = jQuery('#dokuwiki__aside h3.toggle');
+    var $toc = jQuery('#dw__toc h3.toggle');
+    var $tocicon = jQuery('#dw__toc h3.toggle strong');
+
+    if (device_class.match(/extracted/)){
+        // toc expand and aside hide
+        if($toc.length) {
+            $toc[0].setState(1);
+            $tocicon.hide();
+        }
+        if($aside.length) {
+            $aside[0].setState(1);
+            $aside.hide();
+        }
+    }
+    if (device_class == 'desktop') {
+        // reset for desktop mode
+        if($aside.length) {
+            $aside[0].setState(1);
+            $aside.hide();
+        }
+        if($toc.length) {
+            $toc[0].setState(1);
+            $tocicon.show();
+        }
+    }
+    if (device_class.match(/mobile tablet/)){
+        // reset for tablet mode
+        if($aside.length) {
+            $aside[0].setState(1);
+            $aside.hide();
+        }
+        if($toc.length) {
+            $toc[0].setState(1);
+            $tocicon.show();
+        }
+    }
+    if (device_class.match(/mobile phone/)){
+        // toc and sidebar hiding
+        if($aside.length) {
+            $aside.show();
+            $aside[0].setState(-1);
+        }
+        if($toc.length) {
+            $toc[0].setState(-1);
+            $tocicon.show();
+        }
+    }
+}
+
 
 jQuery(function(){
-    //var resizeTimer;
+    var resizeTimer;
     dw_page.makeToggle('#dokuwiki__aside h3.toggle','#dokuwiki__aside div.content');
 
-    //tpl_dokuwiki_mobile();
-    //jQuery(window).on('resize',
-    //    function(){
-    //        if (resizeTimer) clearTimeout(resizeTimer);
-    //        resizeTimer = setTimeout(tpl_dokuwiki_mobile,200);
-    //    }
-    //);
-
-    // increase sidebar length to match content (desktop mode only)
-    //var $sidebar = jQuery('.desktop #dokuwiki__aside');
-    //if($sidebar.length) {
-    //    var $content = jQuery('#dokuwiki__content div.page');
-    //    $content.css('min-height', $sidebar.height());
-    //}
+    mixture_mobile();
+    jQuery(window).on('resize',
+        function(){
+            if (resizeTimer) clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(mixture_mobile,200);
+        }
+    );
 });
